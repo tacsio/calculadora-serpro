462c5f9326a49ca8d025234ee7d2d937478f6c32
Tarcisio Coutinho <tcs5cin@gmail.com>
2023-12-20 17:20:58 -0300

    github badge

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -3,12 +3,21 @@
 import { useState } from "react";
 import Dashboard from "./dashboard";
 import CalcForm from "./form";
+import Image from "next/image";
+import Link from "next/link";
 
 export default function Calculator() {
   const [data, setData] = useState(null);
 
   return (
     <div>
+      <Link
+        href={"https://github.com/tacsio/calculadora-serpro"}
+        className="github_banner"
+      >
+        View on GitHub
+      </Link>
+
       <CalcForm setData={setData} />
       {data && <Dashboard data={data} />}
     </div>
