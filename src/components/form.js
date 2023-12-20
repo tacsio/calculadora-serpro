268b86098dc752742bd9005dada81f77158964d9
Tarcisio Coutinho <tcs5cin@gmail.com>
2023-12-20 15:33:52 -0300

    Update form.js

--- src/components/form.js
+++ src/components/form.js
@@ -20,7 +20,7 @@ export default function CalcForm({ setData }) {
       <div className="space-y-12">
         <div className="border-b border-gray-900/10 pb-2">
           <h2 className="text-base font-semibold leading-7 text-gray-900">
-            SERPRO - Analista PGPS
+            SERPRO - Analista PGCS (8h)
           </h2>
           <p className="mt-1 text-sm leading-6 text-gray-600">
             Simulador de salário
