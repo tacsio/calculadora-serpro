b81fc2539cbf27736f0ec35eb24d29db2907661a
Tarcisio Coutinho <tcs5cin@gmail.com>
2023-12-29 18:30:12 -0300

    change layout

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,7 @@ import Calculator from "@/components/calculator";
 
 export default function Home() {
   return (
-    <main className="flex screen flex-col align-center justify-center p-14">
+    <main className="flex screen flex-col align-center justify-center p-14 md:px-72">
       <Calculator />
     </main>
   );
--- src/components/form.js
+++ src/components/form.js
@@ -129,7 +129,7 @@ export default function CalcForm({ setData }) {
             Simulação
           </h2>
           <p className="mt-1 text-sm leading-6 text-gray-600">
-            Configurações de parâmetros de simulação de reajustes
+            Configurações de parâmetros de simulação
           </p>
 
           <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
