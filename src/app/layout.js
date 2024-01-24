2fb4909b81a1887d1265df8dd03801d995f86236
Tarcisio Coutinho <tcs5cin@gmail.com>
2024-01-24 19:54:43 -0300

    google analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,6 @@
 import { Inter } from 'next/font/google'
+import { GoogleAnalytics } from '@next/third-parties/google'
+ 
 import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -12,6 +14,8 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
+      <GoogleAnalytics gaId="G-DVDJMP7TFP"/>
+      
     </html>
   )
 }
